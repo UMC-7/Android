628d5c0e989bab52a0c883d004da7da374f174ee
modzivv <vvgkxm@gmail.com>
2024-09-28 21:41:44 +0900

    Feat: 1주차 미션

---
+++ Heather/UMCStudy/app/src/main/java/com/example/umcstudy/MainActivity.kt
@@ -0,0 +1,61 @@
+package com.example.umcstudy
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.umcstudy.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // ViewBinding 사용
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        enableEdgeToEdge()
+
+        // findViewById(R.id.main) -> binding.main 변경
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        // 버튼 클릭 이벤트
+        binding.btnHappy.setOnClickListener {
+            moveToSecondActivity("더없이 행복한")
+        }
+        binding.btnExcited.setOnClickListener {
+            moveToSecondActivity("들뜨고 흥분되는")
+        }
+        binding.btnNeutral.setOnClickListener {
+            moveToSecondActivity("평범한")
+        }
+        binding.btnAnxious.setOnClickListener {
+            moveToSecondActivity("생각이 많아지고 불안한")
+        }
+        binding.btnAngry.setOnClickListener {
+            moveToSecondActivity("부글부글 화가 나는")
+        }
+
+        // 뒤로가기
+//        binding.btnBack.setOnClickListener {
+//            finish()
+//        }
+    }
+
+    // SecondActivity로 이동
+    private fun moveToSecondActivity(emotion: String) {
+        val intent = Intent(this, SecondActivity::class.java)
+        intent.putExtra("emotion", emotion)
+        startActivity(intent)
+    }
+
+}
