5cc001e55b9213634d252ef93e73c381c7435b20
yangjiae12 <yangjiae12@gmail.com>
2024-09-29 19:02:27 +0900

    feat: View Binding 도입으로 findViewById 대체

--- naru/W01/app/src/main/java/com/example/w01_mission/MainActivity.kt
+++ naru/W01/app/src/main/java/com/example/w01_mission/MainActivity.kt
@@ -2,49 +2,41 @@ package com.example.w01_mission
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.ImageView
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.w01_mission.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
 
-        val face1: ImageView = findViewById(R.id.ImageView_face1)
-        face1.setOnClickListener {
+        // 뷰 바인딩 객체 초기화
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // 이미지 클릭 시 MainActivity2로 이동
+        binding.ImageViewFace1.setOnClickListener {
             val intent = Intent(this, MainActivity2::class.java)
             startActivity(intent)
         }
 
-        val face2: ImageView = findViewById(R.id.ImageView_face2)
-        face2.setOnClickListener {
+        binding.ImageViewFace2.setOnClickListener {
             val intent = Intent(this, MainActivity2::class.java)
             startActivity(intent)
         }
 
-        val face3: ImageView = findViewById(R.id.ImageView_face3)
-        face3.setOnClickListener {
+        binding.ImageViewFace3.setOnClickListener {
             val intent = Intent(this, MainActivity2::class.java)
             startActivity(intent)
         }
 
-        val face4: ImageView = findViewById(R.id.ImageView_face4)
-        face4.setOnClickListener {
+        binding.ImageViewFace4.setOnClickListener {
             val intent = Intent(this, MainActivity2::class.java)
             startActivity(intent)
         }
 
-        val face5: ImageView = findViewById(R.id.ImageView_face5)
-        face5.setOnClickListener {
+        binding.ImageViewFace5.setOnClickListener {
             val intent = Intent(this, MainActivity2::class.java)
             startActivity(intent)
         }
