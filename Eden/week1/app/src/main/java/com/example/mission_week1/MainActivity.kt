3a0e57bc937526fc72ebab543a632d6474d6c4bf
yc3697 <youngchan9683@gmail.com>
2024-10-02 15:16:41 +0900

    Refactor : 폴더 정리

--- Eden/week1/app/src/main/java/com/example/mission_week1/MainActivity.kt
+++ Eden/week1/app/src/main/java/com/example/mission_week1/MainActivity.kt
@@ -1,65 +1,54 @@
 package com.example.mission_week1
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.mission_week1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        setContentView(R.layout.activity_main)
-
-        val happyImageView : ImageView = findViewById(R.id.happy_iv)
-
-        happyImageView.setOnClickListener{
+        binding.happyIv.setOnClickListener{
             val intent = Intent(this, NextActivity::class.java)
             startActivity(intent)
         }
 
-        val excitedImageView : ImageView = findViewById(R.id.exited_iv)
-
-        excitedImageView.setOnClickListener{
+        binding.exitedIv.setOnClickListener{
             val intent = Intent(this, NextActivity::class.java)
             startActivity(intent)
         }
 
-        val normalImageView : ImageView = findViewById(R.id.normal_iv)
-
-        normalImageView.setOnClickListener{
+        binding.normalIv.setOnClickListener{
             val intent = Intent(this, NextActivity::class.java)
             startActivity(intent)
         }
 
-        val nervousImageView : ImageView = findViewById(R.id.nervous_iv)
-
-        nervousImageView.setOnClickListener{
+        binding.nervousIv.setOnClickListener{
             val intent = Intent(this, NextActivity::class.java)
             startActivity(intent)
         }
 
-        val angryImageView : ImageView = findViewById(R.id.angry_iv)
-
-        angryImageView.setOnClickListener{
+        binding.angryIv.setOnClickListener{
             val intent = Intent(this, NextActivity::class.java)
             startActivity(intent)
         }
-
-
-
-
-
-
     }
 }
