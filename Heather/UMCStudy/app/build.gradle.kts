49fd38546f1bdcec4d8e9e045430448c3ec401c3
modzivv <vvgkxm@gmail.com>
2024-10-07 12:46:49 +0900

    Feat: 2주차 미션

--- Heather/UMCStudy/app/build.gradle.kts
+++ Heather/UMCStudy/app/build.gradle.kts
@@ -52,4 +52,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation("androidx.core:core-splashscreen:1.0.0")
+
 }
