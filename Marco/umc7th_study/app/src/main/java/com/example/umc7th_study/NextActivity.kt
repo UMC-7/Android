bf0402f44721faa5107492caeb5659f73e241827
riadan710 <riadan710@gmail.com>
2024-10-07 16:46:54 +0900

    Feat : 2주차 Fragment 전환시 애니메이션 구현

--- Marco/umc7th_study/app/src/main/java/com/example/umc7th_study/NextActivity.kt
+++ Marco/umc7th_study/app/src/main/java/com/example/umc7th_study/NextActivity.kt
@@ -2,10 +2,14 @@ package com.example.umc7th_study
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import com.example.umc7th_study.databinding.ActivityNextBinding
 
 class NextActivity : AppCompatActivity() {
     private lateinit var binding: ActivityNextBinding
+
+    // 현재 좌표 파악용 (프래그먼트)
+    var cur = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,8 +23,11 @@ class NextActivity : AppCompatActivity() {
             finish()
         }
 
+        // 선택된 좌표 저장
+        cur = intent.getIntExtra("id", 1)
+
         // 바텀 네비게이션 뷰 작동
-        initBottomNavigation(intent.getIntExtra("id", 1))
+        initBottomNavigation(cur)
     }
 
 
@@ -55,31 +62,44 @@ class NextActivity : AppCompatActivity() {
         binding.nextBottomNav.setOnItemSelectedListener { item ->
             when (item.itemId) {
                 R.id.fragment_home -> {
-                    supportFragmentManager.beginTransaction()
-                        .replace(R.id.next_container_fl, HomeFragment())
-                        .commitAllowingStateLoss()
+                    switchFragment(HomeFragment(), 1)
                     return@setOnItemSelectedListener true
                 }
                 R.id.fragment_search -> {
-                    supportFragmentManager.beginTransaction()
-                        .replace(R.id.next_container_fl, SearchFragment())
-                        .commitAllowingStateLoss()
+                    switchFragment(SearchFragment(), 2)
                     return@setOnItemSelectedListener true
                 }
                 R.id.fragment_calender -> {
-                    supportFragmentManager.beginTransaction()
-                        .replace(R.id.next_container_fl, CalenderFragment())
-                        .commitAllowingStateLoss()
+                    switchFragment(CalenderFragment(), 3)
                     return@setOnItemSelectedListener true
                 }
                 R.id.fragment_profile -> {
-                    supportFragmentManager.beginTransaction()
-                        .replace(R.id.next_container_fl, ProfileFragment())
-                        .commitAllowingStateLoss()
+                    switchFragment(ProfileFragment(), 4)
                     return@setOnItemSelectedListener true
                 }
                 else -> false
             }
         }
+    }
+
+    fun switchFragment(fragment: Fragment, dest: Int) {
+        if(cur == dest) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.next_container_fl, fragment)
+                .commitAllowingStateLoss()
+        }
+        else if(cur > dest) {
+            supportFragmentManager.beginTransaction()
+                .setCustomAnimations(R.anim.slide_enter_from_left, R.anim.slide_exit_to_right)
+                .replace(R.id.next_container_fl, fragment)
+                .commitAllowingStateLoss()
+        }
+        else if(cur < dest) {
+            supportFragmentManager.beginTransaction()
+                .setCustomAnimations(R.anim.slide_enter_from_right, R.anim.slide_exit_to_left)
+                .replace(R.id.next_container_fl, fragment)
+                .commitAllowingStateLoss()
+        }
+        cur = dest
     }
 }
