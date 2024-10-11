6737b9d650a2370a931cc366dcbccbed150372a5
yc3697 <youngchan9683@gmail.com>
2024-10-11 23:03:19 +0900

    feat : 2주차 홈에서 엘범 이미지 클릭 시 전환 및 종료 구현

--- Eden/Clone_Coding/app/src/main/java/com/example/clone_coding/HomeFragment.kt
+++ Eden/Clone_Coding/app/src/main/java/com/example/clone_coding/HomeFragment.kt
@@ -19,6 +19,12 @@ class HomeFragment : Fragment() {
     ): View {
         binding = FragmentHomeBinding.inflate(inflater, container, false)
 
+        binding.homeAlbumImgIv1.setOnClickListener {
+            (context as MainActivity).supportFragmentManager.beginTransaction()
+                .replace(R.id.main_frm , AlbumFragment())
+                .commitAllowingStateLoss()
+        }
+
         return binding.root
     }
 }
