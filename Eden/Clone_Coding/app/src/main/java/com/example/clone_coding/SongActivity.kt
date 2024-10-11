0bf9556bf4e2c646ca29e862759a4b594e076b40
yc3697 <youngchan9683@gmail.com>
2024-10-11 20:46:23 +0900

    feat : 2주차 SongActivity 종료, 정지/재생 전환 기능 추가

--- Eden/Clone_Coding/app/src/main/java/com/example/clone_coding/SongActivity.kt
+++ Eden/Clone_Coding/app/src/main/java/com/example/clone_coding/SongActivity.kt
@@ -1,6 +1,7 @@
 package com.example.clone_coding
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +16,29 @@ class SongActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivitySongBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.songDownIb.setOnClickListener {
+            finish()
+        }
+
+        binding.songMiniplayerIv.setOnClickListener {
+            setPlayerStatus(true)
+        }
+
+        binding.songPauseIv.setOnClickListener {
+            setPlayerStatus(false)
+        }
+
+    }
+
+    private fun setPlayerStatus (isPlaying : Boolean){
+        if(isPlaying){
+            binding.songMiniplayerIv.visibility = View.GONE
+            binding.songPauseIv.visibility = View.VISIBLE
+        } else {
+            binding.songMiniplayerIv.visibility = View.VISIBLE
+            binding.songPauseIv.visibility = View.GONE
+        }
 
     }
 }
