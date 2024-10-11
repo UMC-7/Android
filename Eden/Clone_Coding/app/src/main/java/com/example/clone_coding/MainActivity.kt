0377a7f7e5120596b814f34011e986927a8eeb8a
yc3697 <youngchan9683@gmail.com>
2024-10-11 15:45:16 +0900

    feat: BottomNavigationView 이동 및 home fragment 레이아웃 적용

---
+++ Eden/Clone_Coding/app/src/main/java/com/example/clone_coding/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.clone_coding
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.clone_coding.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setTheme(R.style.Theme_FLO)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initBottomNavigation()
+    }
+
+    private fun initBottomNavigation(){
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.main_frm, HomeFragment())
+            .commitAllowingStateLoss()
+
+    }
+}
