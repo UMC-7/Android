70607c44c61b23ae82fdadd131316ccb1a248671
yc3697 <youngchan9683@gmail.com>
2024-10-12 00:32:37 +0900

    feat : 2주차 곡 클릭 시 Toast 메시지 출력 - 라일락

--- Eden/Clone_Coding/app/src/main/java/com/example/clone_coding/AlbumFragment.kt
+++ Eden/Clone_Coding/app/src/main/java/com/example/clone_coding/AlbumFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.example.clone_coding.databinding.FragmentAlbumBinding
 
 class AlbumFragment : Fragment() {
@@ -24,6 +25,11 @@ class AlbumFragment : Fragment() {
                 .replace(R.id.main_frm , HomeFragment())
                 .commitAllowingStateLoss()
         }
+
+        binding.songLalacLayout.setOnClickListener{
+            Toast.makeText(activity, "LILAC", Toast.LENGTH_SHORT).show()
+        }
+
 
 
         return binding.root
