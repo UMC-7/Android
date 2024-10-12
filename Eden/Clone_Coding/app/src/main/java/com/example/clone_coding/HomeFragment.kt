79578bab56e79f44daeafede56b6a8ea0c24344e
yc3697 <youngchan9683@gmail.com>
2024-10-12 11:39:49 +0900

    feat : 3주차 홈 배너 스와이프 기능 구현

--- Eden/Clone_Coding/app/src/main/java/com/example/clone_coding/HomeFragment.kt
+++ Eden/Clone_Coding/app/src/main/java/com/example/clone_coding/HomeFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.viewpager2.widget.ViewPager2
 import com.example.clone_coding.databinding.FragmentHomeBinding
 
 
@@ -24,6 +25,13 @@ class HomeFragment : Fragment() {
                 .replace(R.id.main_frm , AlbumFragment())
                 .commitAllowingStateLoss()
         }
+
+        val bannerAdapter = BannerVPAdapter(this)
+        bannerAdapter.addFragment(BannerFragment(R.drawable.img_home_viewpager_exp))    //이미지 리소스 id값 넘기기
+        bannerAdapter.addFragment(BannerFragment(R.drawable.img_home_viewpager_exp2))
+        bannerAdapter.addFragment(BannerFragment(R.drawable.img_home_viewpager_exp))
+        binding.homeBannerVp.adapter = bannerAdapter
+        binding.homeBannerVp.orientation = ViewPager2.ORIENTATION_HORIZONTAL
 
         return binding.root
     }
