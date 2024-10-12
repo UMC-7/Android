83e3aaebcf9182f32e177793c35e4efadc22fdaf
yangjiae12 <yangjiae12@gmail.com>
2024-10-12 19:56:00 +0900

    feat : 3주차 미션 레이아웃 설정, 액티비티 및 프래그먼트 이동

---
+++ naru/AndroidFloClone/app/src/main/java/com/example/androidfloclone/AlbumFragment.kt
@@ -0,0 +1,30 @@
+package com.example.androidfloclone
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import com.example.androidfloclone.databinding.FragmentAlbumBinding
+
+class AlbumFragment : Fragment() {
+    lateinit var binding: FragmentAlbumBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentAlbumBinding.inflate(inflater,container,false)
+
+        binding.albumBackIv.setOnClickListener {
+            (context as MainActivity).supportFragmentManager.beginTransaction().replace(R.id.main_frm,HomeFragment()).commitAllowingStateLoss()
+        }
+
+        binding.songVioletLayout.setOnClickListener {
+            Toast.makeText(activity,"Violet", Toast.LENGTH_SHORT).show()
+        }
+        return binding.root
+    }
+}
---
+++ naru/AndroidFloClone/app/src/main/java/com/example/androidfloclone/Song.kt
@@ -0,0 +1,6 @@
+package com.example.androidfloclone
+
+data class Song(
+    val title: String = "",
+    val singer: String = ""
+)
