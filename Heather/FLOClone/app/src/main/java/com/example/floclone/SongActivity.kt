aa4fc7bf15541bb13a0ea0a26a0f7bf7b79e5bb8
modzivv <vvgkxm@gmail.com>
2024-10-13 21:58:00 +0900

    Feat: HomeFragment <-> AlbumFragment 간 전환 기능 구현

--- Heather/FLOClone/app/src/main/java/com/example/floclone/SongActivity.kt
+++ Heather/FLOClone/app/src/main/java/com/example/floclone/SongActivity.kt
@@ -1,20 +1,41 @@
 package com.example.floclone
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.floclone.databinding.ActivitySongBinding
 
 class SongActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivitySongBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_song)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        binding = ActivitySongBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // 내리기 버튼 클릭 시 현재 액티비티 종료
+        binding.songDownIb.setOnClickListener {
+            finish()
+        }
+
+        binding.songMiniplayerIv.setOnClickListener {
+            setPlayerStatus(false)
+        }
+        binding.songPauseIv.setOnClickListener {
+            setPlayerStatus(true)
+        }
+    }
+
+    // 재생 버튼의 상태를 바꿔주는 메서드
+    fun setPlayerStatus(isPlaying : Boolean) {
+        if (isPlaying) {
+            binding.songMiniplayerIv.visibility = View.VISIBLE
+            binding.songPauseIv.visibility = View.GONE
+        } else {
+            binding.songMiniplayerIv.visibility = View.GONE
+            binding.songPauseIv.visibility = View.VISIBLE
         }
     }
 }
