63d5063cd2b65b79dae624a4a3af44ddbb72c11e
modzivv <vvgkxm@gmail.com>
2024-10-13 21:06:23 +0900

    Feat: 노래 재생(activity_song), 앨범 정보(fragment_album) XML 레이아웃 작성

--- Heather/FLOClone/app/src/main/java/com/example/floclone/MainActivity.kt
+++ Heather/FLOClone/app/src/main/java/com/example/floclone/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.floclone
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.floclone.databinding.ActivityMainBinding
@@ -12,6 +13,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.mainPlayerCl.setOnClickListener {
+            startActivity(Intent(this, SongActivity::class.java))
+        }
 
         initBottomNavigation()
 
