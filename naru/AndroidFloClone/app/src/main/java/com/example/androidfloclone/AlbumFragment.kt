8da461925c0ba8c7a31be73452c8d689222c2a88
yangjiae12 <yangjiae12@gmail.com>
2024-10-13 11:04:47 +0900

    feat: 3주차 미션 AlbumFragment의 TabLayout 및 Viewpager 구현

--- naru/AndroidFloClone/app/src/main/java/com/example/androidfloclone/AlbumFragment.kt
+++ naru/AndroidFloClone/app/src/main/java/com/example/androidfloclone/AlbumFragment.kt
@@ -7,9 +7,12 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import com.example.androidfloclone.databinding.FragmentAlbumBinding
+import com.google.android.material.tabs.TabLayoutMediator
 
 class AlbumFragment : Fragment() {
     lateinit var binding: FragmentAlbumBinding
+
+    private val information = arrayListOf("수록곡", "상세정보", "영상")
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -19,12 +22,19 @@ class AlbumFragment : Fragment() {
         binding = FragmentAlbumBinding.inflate(inflater,container,false)
 
         binding.albumBackIv.setOnClickListener {
-            (context as MainActivity).supportFragmentManager.beginTransaction().replace(R.id.main_frm,HomeFragment()).commitAllowingStateLoss()
+            (context as MainActivity).supportFragmentManager.beginTransaction()
+                .replace(R.id.main_frm,HomeFragment())
+                .commitAllowingStateLoss()
         }
 
-        binding.songVioletLayout.setOnClickListener {
-            Toast.makeText(activity,"Violet", Toast.LENGTH_SHORT).show()
-        }
+        val albumAdapter = AlbumVPAdapter(this)
+        binding.albumContentVp.adapter = albumAdapter
+
+        TabLayoutMediator(binding.albumContentTb, binding.albumContentVp) {
+            tap, position ->
+            tap.text = information[position]
+        }.attach()
+
         return binding.root
     }
 }
---
+++ naru/AndroidFloClone/app/src/main/java/com/example/androidfloclone/DetailFragment.kt
@@ -0,0 +1,21 @@
+package com.example.androidfloclone
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.androidfloclone.databinding.FragmentDetailBinding
+
+class DetailFragment : Fragment() {
+    lateinit var binding: FragmentDetailBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentDetailBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+}
