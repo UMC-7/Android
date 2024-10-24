4488b4263d711afe5da28d57c89fa45489d5f69e
yc3697 <youngchan9683@gmail.com>
2024-10-24 17:32:58 +0900

    feat : 3주차 반복재생, 랜덤재생 클릭 시 색상 변경 설정

--- Eden/Clone_Coding/app/src/main/java/com/example/clone_coding/SongActivity.kt
+++ Eden/Clone_Coding/app/src/main/java/com/example/clone_coding/SongActivity.kt
@@ -2,17 +2,20 @@ package com.example.clone_coding
 
 import android.app.Activity
 import android.content.Intent
+import android.graphics.PorterDuff
 import android.os.Bundle
 import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.clone_coding.databinding.ActivitySongBinding
 
 class SongActivity : AppCompatActivity() {
 
-    lateinit var binding : ActivitySongBinding
+    private lateinit var binding : ActivitySongBinding
+    private var isColorChanged = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,6 +30,28 @@ class SongActivity : AppCompatActivity() {
 
             finish()
         }
+
+        //반복재생 설정
+        binding.songRepeatIv.setOnClickListener {
+            if (isColorChanged) {
+                binding.songRepeatIv.setColorFilter(ContextCompat.getColor(this, R.color.flo), PorterDuff.Mode.SRC_ATOP)
+            } else {
+                binding.songRepeatIv.setColorFilter(ContextCompat.getColor(this, R.color.colorPrimaryGrey), PorterDuff.Mode.SRC_ATOP)
+            }
+            isColorChanged = !isColorChanged // 상태 반전
+        }
+
+        //랜덤재생 설정
+        binding.songRandomIv.setOnClickListener {
+            if (isColorChanged) {
+                binding.songRandomIv.setColorFilter(ContextCompat.getColor(this, R.color.flo), PorterDuff.Mode.SRC_ATOP)
+            } else {
+                binding.songRandomIv.setColorFilter(ContextCompat.getColor(this, R.color.colorPrimaryGrey), PorterDuff.Mode.SRC_ATOP)
+            }
+            isColorChanged = !isColorChanged // 상태 반전
+        }
+
+
 
         binding.songMiniplayerIv.setOnClickListener {
             setPlayerStatus(true)
