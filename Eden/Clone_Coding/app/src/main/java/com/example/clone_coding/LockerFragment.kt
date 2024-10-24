6e95a1590e6ff0376d4198ce4396ef7741e51062
yc3697 <youngchan9683@gmail.com>
2024-10-24 18:38:58 +0900

    feat : 3주차 보관함 페이지 ViewPager2 구현

--- Eden/Clone_Coding/app/src/main/java/com/example/clone_coding/LockerFragment.kt
+++ Eden/Clone_Coding/app/src/main/java/com/example/clone_coding/LockerFragment.kt
@@ -6,17 +6,28 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.clone_coding.databinding.FragmentLockerBinding
+import com.google.android.material.tabs.TabLayoutMediator
 
 
 class LockerFragment : Fragment() {
 
-    lateinit var binding: FragmentLockerBinding
+    private lateinit var binding: FragmentLockerBinding
+    private var information = arrayListOf("저장한 곡", "음악파일")
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentLockerBinding.inflate(inflater, container, false)
+
+        val lockerAdapter = LockerVPAdapter(this)
+        binding.lockerContentVp.adapter = lockerAdapter
+
+        TabLayoutMediator(binding.lockerContentTb, binding.lockerContentVp){
+            //탭의 제목 설정
+                tab, position -> tab.text = information[position]
+        }.attach()
+
         return binding.root
 
     }
