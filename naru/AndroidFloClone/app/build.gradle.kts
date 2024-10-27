e131771d4c1b4fb06692db2c793f6c56df6b263f
yangjiae12 <yangjiae12@gmail.com>
2024-10-27 15:35:37 +0900

    Feat: 3주차 Indicator 추가 및 자동 슬라이드 구현

--- naru/AndroidFloClone/app/build.gradle.kts
+++ naru/AndroidFloClone/app/build.gradle.kts
@@ -47,6 +47,8 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation (libs.circleindicator)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- naru/AndroidFloClone/app/src/main/java/com/example/androidfloclone/HomeFragment.kt
+++ naru/AndroidFloClone/app/src/main/java/com/example/androidfloclone/HomeFragment.kt
@@ -7,10 +7,17 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.widget.ViewPager2
 import com.example.androidfloclone.databinding.FragmentHomeBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.isActive
+import kotlinx.coroutines.launch
 
 class HomeFragment : Fragment() {
 
     lateinit var binding: FragmentHomeBinding
+    private lateinit var job: Job
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -50,6 +57,27 @@ class HomeFragment : Fragment() {
         panelAdapter.addFragment(PanelFragment(R.drawable.img_first_album_default))
         binding.homePannelBackgroundVp.adapter = panelAdapter
         binding.homePannelBackgroundVp.orientation = ViewPager2.ORIENTATION_HORIZONTAL
+
+        binding.homePannelIndicator.setViewPager(binding.homePannelBackgroundVp)
+
+        startAutoScroll()
+
         return binding.root
+    }
+
+    private fun startAutoScroll() {
+        job = CoroutineScope(Dispatchers.Main).launch {
+            while (isActive) {
+                delay(5000)
+                val nextItem = (binding.homePannelBackgroundVp.currentItem + 1) %
+                        (binding.homePannelBackgroundVp.adapter?.itemCount ?: 1)
+                binding.homePannelBackgroundVp.setCurrentItem(nextItem, true)
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        job.cancel()
     }
 }
