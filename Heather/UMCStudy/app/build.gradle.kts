b8099f05d378ff7be03c0355dafbbfdf4fca3bcd
modzivv <vvgkxm@gmail.com>
2024-11-06 19:13:30 +0900

    Feat: 4주차 미션 Coroutine 기반 스톱워치 기능 구현

--- Heather/UMCStudy/app/build.gradle.kts
+++ Heather/UMCStudy/app/build.gradle.kts
@@ -49,10 +49,16 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+//    implementation(libs.androidx.lifecycle.runtime.android)
+//    implementation(libs.androidx.lifecycle.runtime.desktop)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 
     implementation("androidx.core:core-splashscreen:1.0.0")
+    implementation("androidx.activity:activity-ktx:1.9.2")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.5")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.5")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.9.0")
 
 }
