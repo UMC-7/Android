8ec9832fe8c1e1851cc4a58a88c0b70da1f959d5
yangjiae12 <yangjiae12@gmail.com>
2024-11-10 21:03:39 +0900

    Feat: 5주차 실전 미션 레이아웃 구현 및 SDK 버전 업데이트

--- naru/Week5/app/build.gradle.kts
+++ naru/Week5/app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.week5"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.week5"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
--- naru/Week5/app/src/main/java/com/example/week5/MainActivity.kt
+++ naru/Week5/app/src/main/java/com/example/week5/MainActivity.kt
@@ -9,5 +9,6 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
     }
 }
