f852bc15a43732b5d1730941a341365e33f46a6d
yangjiae12 <yangjiae12@gmail.com>
2024-11-10 21:44:31 +0900

    Feat: 5주차 실전미션 ViewActivity 기능 구현

--- naru/Week5/app/src/main/java/com/example/week5/ViewActivity.kt
+++ naru/Week5/app/src/main/java/com/example/week5/ViewActivity.kt
@@ -5,16 +5,22 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.week5.databinding.ActivityViewBinding
 
 class ViewActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityViewBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_view)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        binding = ActivityViewBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val viewText = intent.getStringExtra("text")
+        binding.viewTv.text = viewText
+
+        binding.closeBtn.setOnClickListener {
+            finish()
         }
     }
 }
