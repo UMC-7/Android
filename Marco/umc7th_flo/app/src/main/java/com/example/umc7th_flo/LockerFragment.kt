0a4883b3fd98dda2f0b53a154f72bcb16e6390c6
riadan710 <riadan710@gmail.com>
2024-11-20 15:22:03 +0900

    [Feat] 3주차 미션 LockerFragment에 TabLayout 구현

--- Marco/umc7th_flo/app/src/main/java/com/example/umc7th_flo/LockerFragment.kt
+++ Marco/umc7th_flo/app/src/main/java/com/example/umc7th_flo/LockerFragment.kt
@@ -6,9 +6,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.umc7th_flo.databinding.FragmentLockerBinding
+import com.google.android.material.tabs.TabLayoutMediator
 
 class LockerFragment : Fragment() {
     lateinit var binding: FragmentLockerBinding
+    private val information = arrayListOf("저장한 곡", "음악파일")
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -16,6 +18,13 @@ class LockerFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentLockerBinding.inflate(inflater, container, false)
+
+        val lockerAdapter = LockerVPAdapter(this)
+        binding.lockerContentVp.adapter = lockerAdapter
+        TabLayoutMediator(binding.lockerContentTb, binding.lockerContentVp){
+                tab, position ->
+            tab.text = information[position]
+        }.attach()
 
         return binding.root
     }
