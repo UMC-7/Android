5047b1c3e529d9b7aee23255de0d70ef4b60dd17
yangjiae12 <yangjiae12@gmail.com>
2024-11-23 11:36:07 +0900

    Fix: 미니 플레이어 클릭 시 SongActivity로 데이터 전달 부분 수정

--- naru/AndroidFloClone/app/src/main/java/com/example/androidfloclone/MainActivity.kt
+++ naru/AndroidFloClone/app/src/main/java/com/example/androidfloclone/MainActivity.kt
@@ -17,35 +17,27 @@ class MainActivity : AppCompatActivity() {
     private var song: Song = Song()
     private var gson: Gson = Gson()
 
-    private val getSongResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-        if (result.resultCode == RESULT_OK) {  // 결과 코드가 RESULT_OK인지 확인
-            // song 액티비티에서 전달한 데이터 가져옴
-            val data = result.data
-
-            // 데이터에서 제목을 추출
-            val songTitle = data?.getStringExtra("SONG_TITLE")
-
-            // 토스트 메시지를 화면에 표시
-            Toast.makeText(this, "제목: $songTitle", Toast.LENGTH_SHORT).show() // 토스트 메시지 표시
-        }
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // 미니 플레이어 클릭 시 songActivity로 이동
-        // 현재 Song 객체의 제목과 가수 전달
         binding.mainPlayerCl.setOnClickListener {
-            val intent = Intent(this, SongActivity::class.java)
-            intent.putExtra("title", song.title)
-            intent.putExtra("singer", song.singer)
-            intent.putExtra("second", song.second)
-            intent.putExtra("playTime", song.playTime)
-            intent.putExtra("isPlaying", song.isPlaying)
-            intent.putExtra("music", song.music)
-            getSongResult.launch(intent)
+            // 제목을 텍스트뷰에서 가져옴
+            val songTitle = binding.mainMiniplayerTitleTv.text.toString()
+            val songSinger = binding.mainMiniplayerSingerTv.text.toString()
+
+            // SongActivity로 이동하는 Intent 생성
+            val intent = Intent(this, SongActivity::class.java).apply {
+                putExtra("title", songTitle)
+                putExtra("singer", songSinger)
+                putExtra("second", song.second)
+                putExtra("playTime", song.playTime)
+                putExtra("isPlaying", song.isPlaying)
+                putExtra("music", song.music)
+            }
+            // SongActivity 시작
+            startActivity(intent)
         }
 
         initBottomNavigation()
