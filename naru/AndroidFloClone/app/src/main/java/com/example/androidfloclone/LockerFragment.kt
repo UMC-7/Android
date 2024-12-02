348580320869402a8d36607255ec61c7cbbcda5e
yangjiae12 <yangjiae12@gmail.com>
2024-12-02 16:03:08 +0900

    Feat: 8주차 로그인 및 로그아웃 기능 구현

--- naru/AndroidFloClone/app/src/main/java/com/example/androidfloclone/LockerFragment.kt
+++ naru/AndroidFloClone/app/src/main/java/com/example/androidfloclone/LockerFragment.kt
@@ -6,6 +6,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.example.androidfloclone.databinding.FragmentLockerBinding
 import com.google.android.material.tabs.TabLayoutMediator
@@ -43,5 +44,39 @@ class LockerFragment : Fragment() {
         }
 
         return binding.root
+    }
+
+    override fun onStart() {
+        super.onStart()
+        initViews()
+    }
+
+    private fun getJwt(): Int{
+        val spf = activity?.getSharedPreferences("auth", AppCompatActivity.MODE_PRIVATE)
+        return spf!!.getInt("jwt", 0)
+    }
+
+    private fun initViews() {
+        val jwt : Int = getJwt()
+        if (jwt == 0) {
+            binding.lockerLoginTv.text = "로그인"
+            binding.lockerLoginTv.setOnClickListener {
+                startActivity(Intent(activity, LoginActivity::class.java))
+            }
+        }
+        else {
+            binding.lockerLoginTv.text = "로그아웃"
+            binding.lockerLoginTv.setOnClickListener {
+                logout()
+                startActivity(Intent(activity, MainActivity::class.java))
+            }
+        }
+    }
+
+    private fun logout() {
+        val spf = activity?.getSharedPreferences("auth", AppCompatActivity.MODE_PRIVATE)
+        val editor = spf!!.edit()
+        editor.remove("jwt")
+        editor.apply()
     }
 }
