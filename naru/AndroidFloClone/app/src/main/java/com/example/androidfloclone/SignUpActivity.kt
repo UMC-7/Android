cbb2f3104e42dcaa129ed1b0417e294bfce24148
yangjiae12 <yangjiae12@gmail.com>
2024-12-02 15:31:25 +0900

    Feat: 8주차 회원가입 기능 구현

--- naru/AndroidFloClone/app/src/main/java/com/example/androidfloclone/SignUpActivity.kt
+++ naru/AndroidFloClone/app/src/main/java/com/example/androidfloclone/SignUpActivity.kt
@@ -1,20 +1,50 @@
 package com.example.androidfloclone
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.androidfloclone.databinding.ActivityLoginBinding
+import com.example.androidfloclone.databinding.ActivitySignUpBinding
 
 class SignUpActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivitySignUpBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_sign_up)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.signUpSignUpBtn.setOnClickListener {
+            signUp()
+            finish()
         }
+    }
+
+    private fun getUser() : User {
+        val email : String = binding.signUpIdEt.text.toString() + "@" + binding.signUpDirectInputEt.text.toString()
+        val pwd : String = binding.signUpPasswordEt.text.toString()
+
+        return User(email, pwd)
+    }
+
+    private fun signUp() {
+        if (binding.signUpIdEt.text.toString().isEmpty() || binding.signUpDirectInputEt.text.toString().isEmpty()) {
+            Toast.makeText(this, "이메일 형식이 잘못되었습니다.", Toast.LENGTH_SHORT).show()
+            return
+        }
+        if (binding.signUpPasswordEt.text.toString() != binding.signUpPasswordCheckEt.text.toString()) {
+            Toast.makeText(this, "비밀번호가 일치하지 않습니다.", Toast.LENGTH_SHORT).show()
+            return
+        }
+        val userDB = SongDatabase.getInstance(this)!!
+        userDB.userDao().insert(getUser())
+
+        val user = userDB.userDao().getUsers()
+        Log.d("SignUpAct", user.toString())
     }
 }
